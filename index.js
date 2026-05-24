468e4ea8bd2a096104ccd3547d4bacc12830267d
devmilon923 <dev.milon923@gmail.com>
2026-05-24 21:07:22 +0600

    refactor: implement recursive customLoop function

--- index.js
+++ index.js
@@ -319,15 +319,25 @@
 // }
 // console.log(array);
 
-let array = [3, 7, 1, 9, 10, 5];
+// let array = [3, 7, 1, 9, 10, 5];
 
-for (let i = 1; i < array.length; i++) {
-  let key = i;
-  let j = i - 1;
-  while (j >= 0 && array[j] > key) {
-    array[j + 1] = array[j];
-    j--;
-  }
-  array[j + 1] = key;
+// for (let i = 1; i < array.length; i++) {
+//   let key = i;
+//   let j = i - 1;
+//   while (j >= 0 && array[j] > key) {
+//     array[j + 1] = array[j];
+//     j--;
+//   }
+//   array[j + 1] = key;
+// }
+// console.log(array);
+// console.log(this);
+
+function customLoop(n, fn) {
+  if (n == 0) return;
+  fn(n);
+  customLoop(n - 1, fn);
 }
-console.log(array);
+customLoop(5, (data) => {
+  console.log(data);
+});
